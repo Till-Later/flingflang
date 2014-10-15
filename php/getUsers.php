442cb4f625e25e3b89f5d7654fdff6c15765b370
Till-Later <Lehmann@MacBook9192.local>
2014-10-15 13:20:31 +0200

    SQLDump hinzugefügt

---
+++ php/getUsers.php
@@ -0,0 +1,19 @@
+<?php
+	$username = "root";
+	$password = "";
+	$host = "localhost";
+	$database = "flingflang";
+	
+	$verbindung = mysql_connect("$host","$username","$password");
+	mysql_select_db("$database");
+		
+	$abfrage = "SELECT ID, Benutzername FROM Benutzer;";
+					
+	$ergebnis = mysql_query($abfrage);
+	while( $row = mysql_fetch_object($ergebnis) )
+	{
+			echo $row->ID."|".$row->Benutzername."|";
+
+	}	
+
+?>
